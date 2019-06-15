910168b6600ee778c015cbc4b866f73cf4bcf273
Ray Stubbs <stubbs.ray@gmail.com>
2019-06-14 21:09:25 -0400

    ...

--- install.sh
+++ install.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-add-apt-repository "deb https://raw.githubusercontent.com/ten-lang/deb-repo/master/ /"
 wget -O - "https://raw.githubusercontent.com/ten-lang/deb-repo/master/ten-lang.pub" | apt-key add -
+add-apt-repository "deb https://raw.githubusercontent.com/ten-lang/deb-repo/master/ /"
 apt update
 
